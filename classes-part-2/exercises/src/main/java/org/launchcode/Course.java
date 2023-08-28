82be0dcb8c80e3c1c64f96d9e3dde847c123df23
Joel Foust <joel.foust@yahoo.com>
2023-08-28 14:43:27 -0400

    Chapter 5 Exercises Complete

--- classes-part-2/exercises/src/main/java/org/launchcode/Course.java
+++ classes-part-2/exercises/src/main/java/org/launchcode/Course.java
@@ -1,17 +1,89 @@
 package org.launchcode;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Course {
     private String topic;
     private Teacher instructor;
     private ArrayList<Student> enrolledStudents;
 
+    public Course(String topic, Teacher instructor, ArrayList<Student> enrolledStudents) {
+
+        this.topic = topic;
+        this.instructor = instructor;
+        this.enrolledStudents = enrolledStudents;
+
+    }
+
+    public void setTopic(String aTopic) {
+
+        this.topic = aTopic;
+
+    }
+
+    public void setInstructor(Teacher aInstructor) {
+
+        this.instructor = aInstructor;
+
+    }
+
+    public void setEnrolledStudents(ArrayList<Student> aEnrolledStudents) {
+
+        this.enrolledStudents = aEnrolledStudents;
+
+    }
+
+    public String getTopic() {
+
+        return this.topic;
+
+    }
+
+    public Teacher getInstructor() {
+
+        return this.instructor;
+
+    }
+
+    public ArrayList<Student> getEnrolledStudents() {
+
+        return this.enrolledStudents;
+
+    }
+
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather than
     //  just the class fields.
+
+    public String toString() {
+
+        return String.format("The %s course is instructed by %s %s and there are currently %d students enrolled", this.topic,
+                this.instructor.getFirstName(), this.instructor.getLastName(), this.enrolledStudents.size());
+
+    }
 
 
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Course objects equal.
+
+    public boolean equals(Object toBeCompared) {
+
+        if (toBeCompared == this) {
+            return true;
+        }
+
+        if (toBeCompared == null) {
+            return false;
+        }
+
+        if (toBeCompared.getClass() != getClass()) {
+            return false;
+        }
+
+        Course theCourse = (Course) toBeCompared;
+        return Objects.equals(theCourse.getTopic(), getTopic()) &&
+                Objects.equals(theCourse.getInstructor(), getInstructor());
+
+    }
 
 }
--- classes-part-2/exercises/src/main/java/org/launchcode/Student.java
+++ classes-part-2/exercises/src/main/java/org/launchcode/Student.java
@@ -30,13 +30,39 @@ public class Student {
 
 
     //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
-//        // Determine the grade level of the student based on numberOfCredits
-//    }
+    public String getGradeLevel(int credits) {
+
+        // Determine the grade level of the student based on numberOfCredits
+         if (credits >= 0 && credits <= 29) {
+
+             return "Freshman";
+
+         } else if (credits >= 30 && credits <= 59) {
+
+             return "Sophomore";
+
+         } else if (credits >= 60 && credits <= 89) {
+
+             return "Junior";
+
+         } else {
+
+             return "Senior";
+
+         }
+
+    }
 
     // TODO: Complete the addGrade method.
     public void addGrade(int courseCredits, double grade) {
+
         // Update the appropriate fields: numberOfCredits, gpa
+        double totalQualityScore = this.gpa * this.numberOfCredits;
+        totalQualityScore += courseCredits * grade;
+
+        this.numberOfCredits += courseCredits;
+        this.gpa += totalQualityScore / this.numberOfCredits;
+
     }
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
@@ -75,6 +101,30 @@ public class Student {
 
     private void setNumberOfCredits(int numberOfCredits) {
         this.numberOfCredits = numberOfCredits;
+    }
+
+    public String toString() {
+
+        return String.format("%s is a %s with %d credits and a GPA of %.2f", this.name,
+                this.getGradeLevel(this.numberOfCredits), this.getNumberOfCredits(), this.getGpa());
+
+    }
+
+    public boolean equals(Object toBeCompared) {
+        if (toBeCompared == this) {
+            return true;
+        }
+
+        if (toBeCompared == null) {
+            return false;
+        }
+
+        if (toBeCompared.getClass() != getClass()) {
+            return false;
+        }
+
+        Student theStudent = (Student) toBeCompared;
+        return theStudent.getStudentId() == getStudentId();
     }
 
     public static void main(String[] args) {
