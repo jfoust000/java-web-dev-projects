dad0f8b172fa7291cdb21c12fe0b841e09cde88c
Joel Foust <joel.foust@yahoo.com>
2023-08-31 11:55:36 -0400

    Java Chapter 6 Unit Testing Studio

--- unit-testing/studio/balanced-brackets/src/main/java/org/launchcode/BalancedBrackets.java
+++ unit-testing/studio/balanced-brackets/src/main/java/org/launchcode/BalancedBrackets.java
@@ -22,21 +22,31 @@ public class BalancedBrackets {
      */
     public static boolean hasBalancedBrackets(String str) {
 
+        // If a complete set of open and closing square brackets, brackets should be 0 (true)
         int brackets = 0;
+
+        // Counter for left bracket
         int leftBracket = 0;
+
+        // Counter for right bracket
         int rightBracket = 0;
 
+        // Loop through each character in the provided string
         for (char ch : str.toCharArray()) {
 
+            // If char is leftBracket, increment leftBracket and brackets
             if (ch == '[') {
 
                 leftBracket++;
                 brackets++;
 
+              // If char is rightBracket, increment rightBracket
             } else if (ch == ']') {
 
                 rightBracket++;
 
+                // For every rightBracket found we decrease leftBracket to indicate a set has been found
+                // if there are equal square brackets to leftBrackets, brackets will end up being 0 or true
                 if (brackets > 0) {
 
                     brackets--;
@@ -45,12 +55,14 @@ public class BalancedBrackets {
             }
         }
 
+        // if leftBracket is not equal to rightBracket, return false because we have incomplete sets of open and closing
+        // brackets
         if (leftBracket != rightBracket) {
 
             return false;
 
         }
-
+        // Else, return true
         return brackets == 0;
 
     }
